7c712189d50fed9348c70b83f4cc58c58e7fa666
Tucker <trblackw@ncsu.edu>
2019-01-24 10:16:46 -0500

    refactored grid layout

--- src/components/RepoGrid.js
+++ src/components/RepoGrid.js
@@ -5,24 +5,25 @@ import styled from "styled-components";
 const RepoGrid = ({ repos }) => (
   <Grid>
     {repos.map((repo, i) => (
-      <li key={repo.name}>
-        <div className="rank">#{i + 1}</div>
-        <ul>
-          <li>
-            <div className="img-container">
-              <img
-                src={repo.owner.avatar_url}
-                alt={`avatar for ${repo.owner.login}`}
-              />
-            </div>
-          </li>
-          <li>
-            <a href={repo.html_url}>{repo.name}</a>
-          </li>
-          <li>@{repo.owner.login}</li>
-          <li>{repo.stargazers_count} stars</li>
-        </ul>
-      </li>
+      <div key={repo.name} className="container p-2">
+        <div className="font-thin text-center">
+          <span>#{i + 1}</span> -- <span>{repo.stargazers_count} stars</span>
+        </div>
+        <div className="container text-center">
+          <div className="img-container text-center mx-auto">
+            <img
+              src={repo.owner.avatar_url}
+              alt={`avatar for ${repo.owner.login}`}
+            />
+          </div>
+
+          <a href={repo.html_url} className="text-black font-bold">
+            {repo.name}
+          </a>
+          <br />
+          <span className="font-thin text-grey-darkest">@{repo.owner.login}</span>
+        </div>
+      </div>
     ))}
   </Grid>
 );
@@ -32,27 +33,19 @@ RepoGrid.propTypes = {};
 export default RepoGrid;
 
 const Grid = styled("div")`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-around;
-
-  .rank {
-    font-size: 20px;
-    margin: 10px;
-  }
-
-  ul {
-    list-style: none;
-  }
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  list-style: none;
 
   .img-container {
     width: 100px;
     height: 100px;
-    margin: 0 auto;
+    margin: 1em auto;
   }
 
   img {
     width: 150px;
     border-radius: 50%;
+    object-fit: cover;
   }
 `;
