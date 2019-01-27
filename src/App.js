2ff4e169f07dfa87a6b4dbc65bca4b17a9651b48
Tucker <trblackw@ncsu.edu>
2019-01-26 20:36:39 -0500

    passing currentRepo context to app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Router } from "@reach/router";
 import Nav from "./components/Nav";
 import Landing from "./components/Landing";
+import Repo from "./components/Repo";
+
+//context
+import { Provider } from "./state/context";
+const [{ currentRepo }, dispatch] = useContext(RepoContext);
 
 const App = () => (
-  <>
+  <Provider value={{ currentRepo, dispatch }}>
     <Nav />
     <Router>
       <Landing path="/" />
+      <Repo path="/:repoID" />
     </Router>
-  </>
+  </Provider>
 );
 
 export default App;
