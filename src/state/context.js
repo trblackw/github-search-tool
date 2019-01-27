06f5b8c58ec366ebd46f5dcad27e9bf0b81af82f
Tucker <trblackw@ncsu.edu>
2019-01-27 13:18:11 -0500

    added currentRepo to instance of context

--- src/state/context.js
+++ src/state/context.js
@@ -1,11 +1,13 @@
 import { createContext } from "react";
 
 const RepoContext = createContext({
-  //check to see if there is a repo in localstorage
-  currentRepo: localStorage.getItem("repo")
-    ? JSON.parse(localStorage.getItem("repo"))
+  //check to see if repos or currentRepo exist in localStorage
+  repos: localStorage.getItem("repos")
+    ? JSON.parse(localStorage.getItem("repos"))
+    : [],
+  currentRepo: localStorage.getItem("currentRepo")
+    ? JSON.parse(localStorage.getItem("currentRepo"))
     : {}
 });
-
 
 export default RepoContext;
