c8e2adbbe127b0badd8383f3ed7bda4ca41d3794
Tucker <trblackw@ncsu.edu>
2019-01-27 13:16:00 -0500

    hooked up the context and reducer hooks for state management of app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React, { useContext } from "react";
+import React, { useContext, useReducer } from "react";
 import { Router } from "@reach/router";
 import Nav from "./components/Nav";
 import Landing from "./components/Landing";
 import Repo from "./components/Repo";
+import RepoContext from "./state/context";
+import RepoReducer from "./state/reducer";
 
-//context
-import { Provider } from "./state/context";
-const [{ currentRepo }, dispatch] = useContext(RepoContext);
-
-const App = () => (
-  <Provider value={{ currentRepo, dispatch }}>
-    <Nav />
-    <Router>
-      <Landing path="/" />
-      <Repo path="/:repoID" />
-    </Router>
-  </Provider>
-);
+const App = () => {
+   const initialState = useContext(RepoContext);
+   //deconstruct currentRepo and repos from state returned from useReducer hook
+  const [{ currentRepo, repos }, dispatch] = useReducer(
+    RepoReducer,
+    initialState
+  );
+   //pass currentRepo, repos and dispatch function to app
+  return (
+    <RepoContext.Provider value={{ repos, currentRepo, dispatch }}>
+      <Nav />
+      <Router>
+        <Landing path="/" />
+        <Repo path="/:repoID" />
+      </Router>
+    </RepoContext.Provider>
+  );
+};
 
 export default App;
