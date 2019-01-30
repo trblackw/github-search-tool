ee70c8a0c2973d2f7ccaae0ca6fec7952cdb7b59
Tucker <trblackw@ncsu.edu>
2019-01-30 14:37:41 -0500

    added more details to repo view

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -2,9 +2,11 @@ import React, { useEffect, useContext } from "react";
 import RepoContext from "../state/context";
 import { selectRepo } from "../state/actions";
 import styled from "styled-components";
+import useToggle from '../hooks/useToggle';
 
 const Repo = ({ repoID }) => {
-  const { currentRepo, repos, dispatch } = useContext(RepoContext);
+   const { currentRepo, repos, dispatch } = useContext(RepoContext);
+   const [open, setOpen] = useToggle(false);
   useEffect(
     () => {
       //dispatch the selectRepo action creator
@@ -24,20 +26,24 @@ const Repo = ({ repoID }) => {
     open_issues,
     stargazers_count,
     ssh_url,
-    watchers
+    watchers,
+    owner
   } = currentRepo;
   return (
-    <div className="container max-w-md mx-auto p-3 border border-red-light rounded shadow-md bg-grey-light mt-3">
-      <h1 className="ml-2 text-5xl">{name}</h1>
+    <div className="container max-w-md mx-auto p-5 border rounded shadow-md bg-grey-light mt-3">
+        <h1 className="ml-2 text-5xl"> <span><small className='font-thin'>{owner.login}</small>/</span>{name}</h1>
       <span className="ml-3 text-grey-dark">{language}</span>
       <Description>
-        <p className="font-thin">{description}</p>
+        <p className="font-thin text-center">{description}</p>
       </Description>
-      <hr />
-      <StyledList>
-        <label htmlFor="issues">Open Issues</label>
-        <li name="issues">{open_issues}</li>
-      </StyledList>
+        <hr />
+        <button className='bg-white hover:bg-grey-lightest text-grey-darkest font-semibold py-2 px-4 border border-grey-light rounded shadow' onClick={() => setOpen(!open)}>{open ? "Hide" : "Show"} Details</button>
+      {open && <StyledList className="bg-grey-lighter">
+        <label htmlFor="issues" className="font-bold">Open Issues</label>
+           <li name="issues" className='mb-4'>{open_issues}</li>
+           <label htmlFor="forks" className="font-bold">Forks</label>
+        <li name="forks">{forks}</li>
+      </StyledList>}
     </div>
   );
 };
