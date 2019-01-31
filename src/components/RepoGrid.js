dcad9eb40ef211d61bf46960efcad1d76f7d4e75
Tucker <trblackw@ncsu.edu>
2019-01-31 18:15:29 -0500

    utilizing modal with toggle functionality

--- src/components/RepoGrid.js
+++ src/components/RepoGrid.js
@@ -2,33 +2,41 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "@reach/router";
 import styled from "styled-components";
+import Modal from "./Modal";
+import useToggle from "../hooks/useToggle";
 
-const RepoGrid = ({ repos }) => (
-  <Grid>
-    {repos.map((repo, i) => (
-      <div key={repo.id} className="container p-2">
-        <div className="font-thin text-center">
-          <span>#{i + 1}</span> -- <span>{repo.stargazers_count} stars</span>
-        </div>
-        <div className="container text-center">
-          <div className="img-container text-center mx-auto">
-            <img
-              src={repo.owner.avatar_url}
-              alt={`avatar for ${repo.owner.login}`}
-            />
+const RepoGrid = ({ repos }) => {
+   const [on, setOn] = useToggle(false);
+  return (
+    <Grid>
+      {repos.map((repo, i) => (
+        <div key={repo.id} className="container p-2" onClick={() => setOn(true)}>
+          <div className="font-thin text-center">
+            <span>#{i + 1}</span> -- <span>{repo.stargazers_count} stars</span>
           </div>
-          <Link to={`/${repo.id}`} className="text-black font-bold">
-            {repo.name}
-          </Link>
-          <br />
-          <span className="font-thin text-grey-darkest">
-            @{repo.owner.login}
-          </span>
+          <div className="container text-center">
+            <div className="img-container text-center mx-auto">
+              <img
+                src={repo.owner.avatar_url}
+                alt={`avatar for ${repo.owner.login}`}
+              />
+            </div>
+            <Link to={`/${repo.id}`} className="text-black font-bold">
+              {repo.name}
+            </Link>
+            <br />
+            <span className="font-thin text-grey-darkest">
+              @{repo.owner.login}
+            </span>
+            </div>
+            {on && <Modal open={on} toggle={setOn}>
+               will this work?
+            </Modal>}
         </div>
-      </div>
-    ))}
-  </Grid>
-);
+      ))}
+    </Grid>
+  );
+};
 
 RepoGrid.propTypes = {
   repos: PropTypes.arrayOf(PropTypes.object).isRequired
